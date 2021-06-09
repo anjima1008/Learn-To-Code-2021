52a6655e26c041d7f60a75603fe57795481225b5
anjima1008 <62937593+anjima1008@users.noreply.github.com>
2021-06-09 17:25:53 +0530

    Create Program1.c

---
+++ Program1.c
@@ -0,0 +1,57 @@
+Write a C Program to print "Hello World!" and "Welcome to Learn To Code!" in two separate lines
+
+Input Format
+
+None
+
+Constraints
+
+None
+
+Output Format
+
+"Hello World! Welcome to Learn To Code!"
+
+Format these lines as per below output
+Sample Input 0
+
+None
+Sample Output 0
+
+Hello World!
+Welcome to Learn To Code!
+
+
+Explanation 0
+
+The '\n' is an escape sequence known as "Newline".
+
+    
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+   printf("Hello World!\n");
+   printf ("Welcome to Learn To Code!");   
+
+    return 0;
+}
+
+Input (stdin)
+
+None
+Your Output (stdout)
+
+Hello World!
+Welcome to Learn To Code!
+Expected Output
+
+Hello World!
+Welcome to Learn To Code!
+
